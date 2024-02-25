45f152256c9e0b659a0214ae4e2fe9a9c694267e
Muhammad Baasil Irfan Rizvi <baasilrazriz@gmail.com>
2024-02-25 23:19:05 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text >Hi I am Muhammad Basil Irfan</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
